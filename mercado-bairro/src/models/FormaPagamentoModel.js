d8bc408b988761b077ea1662313263659d4146f9
NatanaelTRodrigues <natastavaresrodrigues@gmail.com>
2025-11-20 13:02:58 +0000

    Revert "Subindo arquivos controllers/models/validators"
    This reverts commit fc98c7ff5a5c8e8a0902d6ec3d936fd741512dca.

--- mercado-bairro/src/models/FormaPagamentoModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-  nome: { type: String, required: true },
-  descricao: { type: String, required: true }
-}, { timestamps: true });
-module.exports = mongoose.model('FormasPagamento', schema);
--- mercado-bairro/src/models/FornecedorModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const schema = new mongoose.Schema({
-  nome: { type: String, required: true },
-  cnpj: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
-  telefone: { type: String, required: true }
-}, { timestamps: true });
-module.exports = mongoose.model('Fornecedores', schema);
